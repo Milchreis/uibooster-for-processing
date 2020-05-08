be885bff90882fe0ee6940a506cbf8f4518acc42
Nick <nick.mueller@uni-greifswald.de>
2020-05-08 11:12:38 +0200

    Adds javadoc

---
+++ reference/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"uibooster"},{"l":"uibooster.components"},{"l":"uibooster.model"},{"l":"uibooster.model.formelements"},{"l":"uibooster.utils"}]
