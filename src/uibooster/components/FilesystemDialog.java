f48a962aba7c21a796947199ae3302c3777927d1
PlayStateWolff <playstatewolf@gmail.com>
2021-12-11 15:13:34 -0800

    Additional Changes for File/Folder Entry Points
    Added support for custom entry points in File/Folder Dialog Boxes

--- src/uibooster/components/FilesystemDialog.java
+++ src/uibooster/components/FilesystemDialog.java
@@ -28,10 +28,48 @@ public class FilesystemDialog {
     public static File showFileOrDirectorySelectionDialog(String description, String... extensions) {
         return showFsSelectionDialog(FILES_AND_DIRECTORIES, description, extensions);
     }
+    
+    public static File showFileSelectionDialogFromPath(String path) {
+        return showFsSelectionDialogFromPath(path, FILES_ONLY, null, null);
+    }
+    
+    public static File showFileSelectionDialogFromPath(String path, String description, String... extensions) {
+        return showFsSelectionDialogFromPath(path, FILES_ONLY, description, extensions);
+    }
+    
+    public static File showDirectorySelectionDialogFromPath(String path) {
+        return showFsSelectionDialogFromPath(path, JFileChooser.DIRECTORIES_ONLY, null, null);
+    }
+    
+    public static File showFileOrDirectorySelectionDialogFromPath(String path) {
+        return showFsSelectionDialogFromPath(path, FILES_AND_DIRECTORIES, null, null);
+    }
+    
+    public static File showFileOrDirectorySelectionDialogFromPath(String path, String description, String... extensions) {
+        return showFsSelectionDialogFromPath(path, FILES_AND_DIRECTORIES, description, extensions);
+    }
 
     private static File showFsSelectionDialog(int type, String description, String... extensions) {
 
         JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(type);
+
+        if ((type == FILES_ONLY || type == FILES_AND_DIRECTORIES) && extensions != null && extensions.length > 0) {
+            chooser.setFileFilter(new FileNameExtensionFilter(description, extensions));
+        }
+
+        int returnVal = chooser.showOpenDialog(null);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            return chooser.getSelectedFile();
+        }
+
+        return null;
+    }
+    
+    private static File showFsSelectionDialogFromPath(String path, int type, String description, String... extensions) {
+
+        JFileChooser chooser = new JFileChooser(path);
         chooser.setFileSelectionMode(type);
 
         if ((type == FILES_ONLY || type == FILES_AND_DIRECTORIES) && extensions != null && extensions.length > 0) {
